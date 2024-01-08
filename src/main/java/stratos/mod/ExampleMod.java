c0317a1edd3f887dfaed4a25ddbe06656e53f1f6
Phoenics-space <136085461+Phoenics-space@users.noreply.github.com>
2024-01-08 17:26:25 +0900

    shingle stuff

--- src/main/java/stratos/mod/ExampleMod.java
+++ src/main/java/stratos/mod/ExampleMod.java
@@ -1,11 +1,19 @@
 package stratos.mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stratos.Items.ModItemGroups;
 import stratos.Items.ModItems;
 import stratos.block.ModBlocks;
+import stratos.fluid.BoilingMudFLuid;
 import stratos.particle.ModParticles;
 import stratos.util.ModRegistries;
 import stratos.world.gen.ModWorldGeneration;
@@ -32,10 +40,22 @@ public class ExampleMod
 
         ModWorldGeneration.generateModWorldGeneration();
 
-        /* The Portal is now under: 
+        /* The Portal is now under:
         * 
         * stratos/util/ModRegistries/registerPortal()
         * 
         * */
+
+        STILL_BOILING_MUD = Registry.register(Registries.FLUID, new Identifier("stratos", "acid"), new BoilingMudFLuid.Still());
+        FLOWING_BOILING_MUD = Registry.register(Registries.FLUID, new Identifier("stratos", "flowing_acid"), new BoilingMudFLuid.Flowing());
+        BOILING_MUD_BUCKET = Registry.register(Registries.ITEM, new Identifier("stratos", "acid_bucket"),
+                new BucketItem(STILL_BOILING_MUD, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1)));
+
     }
-}
+
+    public static FlowableFluid STILL_BOILING_MUD;
+    public static FlowableFluid FLOWING_BOILING_MUD;
+    public static Item BOILING_MUD_BUCKET;
+
+    }
+
